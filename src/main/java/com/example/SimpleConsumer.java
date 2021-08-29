c64dba26cf5211d78a2f56fef7659c6864d08535
Jun Kang <jundol8791@gmail.com>
2021-08-29 17:49:21 +0900

    리밸런스 리스너를 이용해 리밸런스가 발생하기 직전에 오프셋 커밋하기

--- src/main/java/com/example/SimpleConsumer.java
+++ src/main/java/com/example/SimpleConsumer.java
@@ -7,10 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 
 public class SimpleConsumer {
@@ -18,6 +15,9 @@ public class SimpleConsumer {
     private final static String TOPIC_NAME = "test";
     private final static String BOOTSTRAP_SERVERS = "my-linux:9092";
     private final static String GROUP_ID = "test-group";
+
+    private static KafkaConsumer<String, String> consumer = null;
+    private static Map<TopicPartition, OffsetAndMetadata> currentOffsets = new HashMap<>();
 
     public static void main(String[] args) {
         Properties configs = new Properties();
@@ -27,28 +27,32 @@ public class SimpleConsumer {
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false); // 명시적 오프셋 커밋을 하겠다는 설정
 
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
-        consumer.subscribe(Arrays.asList(TOPIC_NAME));
+        consumer = new KafkaConsumer<>(configs);
+        consumer.subscribe(Arrays.asList(TOPIC_NAME), new ReblanceListener());
 
         while(true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
 
             for (ConsumerRecord<String, String> record : records) {
                 logger.info("==> {}", record);
-                consumer.commitAsync(new OffsetCommitCallback() {
-                    @Override
-                    public void onComplete(Map<TopicPartition, OffsetAndMetadata> offsets, Exception e) {
-                        if (e != null) {
-                            System.err.println("Commit failed");
-                        } else {
-                            System.out.println("Commit succeded");
-                        }
-                        if (e != null) {
-                            logger.error("Commit failed for offsets {}", offsets, e);
-                        }
-                    }
-                });
+                currentOffsets.put(new TopicPartition(record.topic(), record.partition()),
+                        new OffsetAndMetadata(record.offset() + 1 , null));
+                consumer.commitSync(currentOffsets);
             }
         }
+    }
+
+    public static class ReblanceListener implements ConsumerRebalanceListener {
+        @Override
+        public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+            logger.warn("Partitions are assigned");
+        }
+
+        @Override
+        public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+            logger.warn("Partitions are revoked");
+            consumer.commitSync(currentOffsets);
+        }
+
     }
 }
